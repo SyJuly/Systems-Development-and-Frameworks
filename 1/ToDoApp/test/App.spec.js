eabe81df1b688a858e16152fac6c3d3b3100e106
Eva.Obersheimer <s0558566@htw-berlin.de>
2019-11-10 22:56:53 +0100

    test for app component

---
+++ 1/ToDoApp/test/App.spec.js
@@ -0,0 +1,16 @@
+import { mount} from '@vue/test-utils'
+import {shallowMount} from '@vue/test-utils'
+import App from '../src/App'
+
+describe('Component', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(App)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  it('is rendering welcome-heading', () =>{
+    const wrapper=shallowMount(App)
+    expect(wrapper.find('h1').text()).toEqual('Simple Todo App');
+  })
+})
+
+
