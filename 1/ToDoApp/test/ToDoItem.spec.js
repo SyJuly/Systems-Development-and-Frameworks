7cdd40ccd04447da8b0a4e6e611fe36bb9b994db
Eva.Obersheimer <s0558566@htw-berlin.de>
2019-11-11 15:11:34 +0100

    test update

--- 1/ToDoApp/test/ToDoItem.spec.js
+++ 1/ToDoApp/test/ToDoItem.spec.js
@@ -6,35 +6,36 @@ describe('ToDoItem', () => {
   describe('given a todo', () => {
     const wrapper = mount(ToDoItem, {
       propsData: {
-        todo:{text: 'test-todo'}
+        todo:{
+          text: 'test-todo'
+        }
       }
     })
     it('renders todo.text', () => {
       expect(wrapper.vm.todo.text).toBe('test-todo');
     })
-    it('adds new todo', () => {
-      wrapper.vm.$emit('todo','new task')
-      expect(wrapper.emitted().todo).toBeTruthy()
-      expect(wrapper.emitted().todo[0]).toEqual(['new task']);
-    })
+
     describe('click on Edit Button', () => {
       const editButton=wrapper.find('#editButton');
       it('shows input field', () => {
-        editButton.trigger('click')
+        expect(wrapper.find('#editButton').exists()).toBe(true);
+        editButton.trigger('click');
         expect(wrapper.vm.editModeOn).toBe(true);
       })
       describe('edit text and submit', () => {
         it('$emits save with edited todo', () => {
-          const updatedTodoText='updated todo'
-          wrapper.vm.$emit('todo','update')
+          const updatedTodoText='updated todo';
+          wrapper.vm.$emit('editTodo', updatedTodoText);
+          expect(wrapper.emitted().editTodo[0]).toEqual([updatedTodoText]);
 
         })
       })
       describe('click on remove Button', () => {
         const removeButton=wrapper.find('#removeButton');
         it('$emits remove', () => {
-          expect(wrapper.emitted('removeTodo'));
-        //  expect(wrapper.vm.editModeOn).toBe(false);
+          expect(wrapper.find('#removeButton').exists()).toBe(true);
+          removeButton.trigger('click');
+          expect(wrapper.emitted('removeTodo').toBeTruthy);
         })
       })
     })
