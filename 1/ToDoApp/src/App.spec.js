f8ec4b62a273a98778c89e4df37c7af6b32b8f2d
Eva.Obersheimer <s0558566@htw-berlin.de>
2019-11-18 11:39:01 +0100

    "test" -> "it" vereinheitlichen

--- 1/ToDoApp/src/App.spec.js
+++ 1/ToDoApp/src/App.spec.js
@@ -3,7 +3,7 @@ import {shallowMount} from '@vue/test-utils'
 import App from './App'
 
 describe('Component', () => {
-  test('is a Vue instance', () => {
+  it('is a Vue instance', () => {
     const wrapper = mount(App)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
