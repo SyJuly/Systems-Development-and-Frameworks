61fadd91f8bec9ef7f96cc9da5ff42631c033691
Juliane Fink <jule-fink@gmx.de>
2019-12-16 12:05:54 +0100

    move data from resolvers to db directory

---
+++ ToDoApp/Backend/src/db/data.js
@@ -0,0 +1,30 @@
+let todos = [{
+  id: 1,
+  message: 'first todo',
+  finished: false,
+  creator: 1
+},
+  {
+    id: 2,
+    message: 'second todo',
+    finished: true,
+    creator: 2
+  },
+];
+
+const users = [{
+  id: 1,
+  name: 'testuser',
+  email: 'your@email.com',
+  password: '$2y$10$3IUx11G0mcJfSpFWn3Lru.xac9OqHDzqLOAhdZovaUyKa2DhgCaOS' // "password"
+},
+  {
+    id: 2,
+    name: '2. testuser',
+    email: '2your@email.com',
+    password: '$2y$10$3IUx11G0mcJfSpFWn3Lru.xac9OqHDzqLOAhdZovaUyKa2DhgCaOS' // "password"
+  }
+]
+
+//module.exports.todos = todos;
+//module.exports.users = users;
