d2b99b0103c8b2cebe57d25fa76dac274b90fffb
Juliane Fink <jule-fink@gmx.de>
2019-12-16 23:44:52 +0100

    fix updateTodo query

--- ToDoApp/Backend/src/resolvers/todo/todoResolver.js
+++ ToDoApp/Backend/src/resolvers/todo/todoResolver.js
@@ -45,37 +45,21 @@ const todoResolver = {
           const decoded = jwt.verify(token, CONFIG.JWT_SECRET)
           const userId = decoded.id
           const session = context.driver.session();
-          let updatedTodo = {};
-          try {
-            const queryResults = await session.run(`MATCH (t${id}:Todo{id:${id}}) RETURN t${id}`);
-            const todo = queryResults.records.map(todo => todo.get(`t${id}`).properties)[0];
-            /*if (todo.creator !== userId) {
-              throw new Error(`Your are not the creator of todo:  id ${id}`);
-            }*/
+          const queryResults = await session.run(`MATCH (t${id}:Todo{id:${id}}) RETURN t${id}`);
+          const todo = queryResults.records.map(todo => todo.get(`t${id}`).properties)[0];
+          /*if (todo.creator !== userId) {
+            throw new Error(`Your are not the creator of todo:  id ${id}`);
+          }*/
 
-            updatedTodo = {...todo};
-            updatedTodo.message = message;
-            updatedTodo.finished = finished;
-          } finally{
-            session.close();
-          }
-
-          const updateTodoSession = context.driver.session();
-          try {
-            console.log(updatedTodo);
-            await updateTodoSession.run(
-                'MATCH (t:Todo{id:$id}) \n' +
-                'SET t = {id: $id, message: $message, finished:$finished}',
-                {
-                  id:updatedTodo.id,
-                  message: updatedTodo.message,
-                  finished: updatedTodo.finished
-                }
-            );
-          } finally {
-            updateTodoSession.close()
-          }
-
+          let updatedTodo = {...todo};
+          updatedTodo.message = message;
+          updatedTodo.finished = finished;
+          await session.run(
+              'MATCH (t:Todo{id:$id}) \n' +
+              'SET t = {id: $id, message: $message, finished:$finished}',
+              updatedTodo
+          )
+          session.close();
           return updatedTodo;
         },
         deleteTodo: (_, {id, token}) => {
