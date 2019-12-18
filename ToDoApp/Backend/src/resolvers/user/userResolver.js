af4336f48218b1a2b77b301d10c550d306475f61
Eva.Obersheimer <s0558566@htw-berlin.de>
2019-12-18 12:02:40 +0100

    userResolvers -> neo4J

--- ToDoApp/Backend/src/resolvers/user/userResolver.js
+++ ToDoApp/Backend/src/resolvers/user/userResolver.js
@@ -1,47 +1,82 @@
-const { find }= require('lodash');
-const { generateUUID }= require("../../../utils.js");
-const { CONFIG }= require("../../config/config");
+const {
+    find
+} = require('lodash');
+const {
+    generateUUID
+} = require("../../../utils.js");
+const {
+    CONFIG
+} = require("../../config/config");
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const { neo4jgraphql } = require('neo4j-graphql-js');
-const {users} = require('../../db/data')
+const {
+    neo4jgraphql
+} = require('neo4j-graphql-js');
+const {
+    users
+} = require('../../db/data')
 
 
 const userResolver = {
-  Query: {
-    allUsers(object, params, context, resolveInfo) {
-      return neo4jgraphql(object, params, context, resolveInfo);
-    }
-  },
-  Mutation: {
-    signup: (_, {name, email, password}) => {
-      const userID = generateUUID()
-      const userFromEmail = find(users, {email: email});
-      if (userFromEmail) {
-        throw new Error(`This email is already used by another user: ${email}`);
-      }
-      users.push({
-        id: userID,
-        name: name,
-        email: email,
-        password: bcrypt.hash(password, 10),
-      });
-      const token = jwt.sign({id: userID, email: email}, CONFIG.JWT_SECRET, {expiresIn: '1y'});
-      return token;
+    Query: {
+        allUsers(object, params, context, resolveInfo) {
+            return neo4jgraphql(object, params, context, resolveInfo);
+        }
     },
-    login: (_, {email, password}) => {
-      const userFromEmail = find(users, {email: email});
-      if (!userFromEmail) {
-        throw new Error(`Couldn’t find a user with the email: ${email}`);
-      }
-      const valid = bcrypt.compare(password, userFromEmail.password);
-      if (!valid) {
-        throw new Error('Incorrect password');
-      }
-      const token = jwt.sign({id: userFromEmail.id, email: userFromEmail.email}, CONFIG.JWT_SECRET, {expiresIn: '1y'});
-      return token;
+    Mutation: {
+        signup: async (_, {
+            name,
+            email,
+            password
+        }, context) => {
+            const userID = generateUUID();
+            const session = context.driver.session();
+            const queryResults = await session.run(
+                'MATCH (u:User {email:$email}) RETURN u', {
+                    email
+                }
+            );
+            const userFromEmail = queryResults.records.map(user => user.get(`u`).properties)[0];
+            if (userFromEmail != null) {
+                throw new Error(`This email is already used by another user: ${email}`);
+            } else {
+                await session.run(
+                    'CREATE (u:User {id: $id, name: $name,email: $email ,password: $password }) RETURN u', {
+                        id: userID,
+                        name: name,
+                        email: email,
+                        password: bcrypt.hash(password, 10).toString()
+                    }
+                );
+            }
+            session.close();
+            const token = jwt.sign({id: userID,email: email}, CONFIG.JWT_SECRET, {expiresIn: '1y' });
+            return token;
+        },
+        login: async (_, { email, password  }, context) => {
+            const session = context.driver.session();
+            const queryResults = await session.run(
+                'MATCH (u:User {email:$email}) RETURN u', {
+                    email
+                }
+            );
+            const userFromEmail = queryResults.records.map(user => user.get(`u`).properties)[0];
+            if (userFromEmail == null) {
+                throw new Error(`Couldn’t find a user for email: ${email}`);
+            }
+            const valid = bcrypt.compare(password, userFromEmail.password);
+            if (!valid) {
+                throw new Error('Incorrect password');
+            }
+            const token = jwt.sign({
+                id: userFromEmail.id,
+                email: userFromEmail.email
+            }, CONFIG.JWT_SECRET, {
+                expiresIn: '1y'
+            });
+            return token;
+        }
     }
-  }
-};
+}
 
 module.exports.userResolver = userResolver;
