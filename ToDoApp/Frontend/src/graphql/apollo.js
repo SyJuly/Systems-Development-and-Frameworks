d3aed7f9620c494801fd479336d19b3364a71555
Juliane Fink <jule-fink@gmx.de>
2020-01-02 14:44:01 +0100

    set authorization header in frontend

--- ToDoApp/Frontend/src/graphql/apollo.js
+++ ToDoApp/Frontend/src/graphql/apollo.js
@@ -4,9 +4,10 @@ import { HttpLink } from 'apollo-link-http'
 import { onError } from "apollo-link-error"
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
+import { setContext } from 'apollo-link-context';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000'
+  uri: process.env.VUE_APP_GRAPHQL_ENDPOINT
 })
 
 // Error Handling
@@ -20,9 +21,17 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (networkError) console.log(`[Network error]: ${networkError}`)
 })
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('auth-token');
+  // return the headers to the context so httpLink can read them
+  headers.authorization = token ? token : ""
+  return headers;
+});
+
 // Create the apollo client
 export const apolloClient = new ApolloClient({
-  link: errorLink.concat(httpLink),
+  link: errorLink.concat(authLink.concat(httpLink)),
   cache: new InMemoryCache(),
   connectToDevTools: true
 })
