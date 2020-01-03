bbf2899ebd4399261e36c2341feb940e545ede2d
Juliane Fink <jule-fink@gmx.de>
2020-01-03 03:38:19 +0100

    fix checking on empty auth obj in context

--- ToDoApp/Backend/src/config/permissions.js
+++ ToDoApp/Backend/src/config/permissions.js
@@ -1,7 +1,8 @@
 const { rule, shield, not } =  require('graphql-shield');
+const { isEmptyObject } =  require('../utils/helpers');
 
 const isAuthenticated = rule()(async (parent, args, context) => {
-  return context.auth !== null && context.auth !== undefined && context.auth !== {}
+  return context.auth !== null && context.auth !== undefined && !isEmptyObject(context.auth)
 });
 
 const permissions = shield({
